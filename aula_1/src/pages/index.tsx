a9ca9e86dd20632ff9d9386300fff94a38947aeb
Div <72998710+DIEGOHORVATTI@users.noreply.github.com>
2022-12-11 03:23:04 +0000

    fix: aula_2

--- aula_1/src/pages/index.tsx
+++ aula_1/src/pages/index.tsx
@@ -1,5 +1,9 @@
-import Home from '@pages/home'
+import Aula_1 from '@pages/aula_1'
+import Aula_2 from '@pages/aula_2'
 
 export default function index() {
-  return <Home />
+  return (
+    // <Aula_1 />
+    <Aula_2 />
+  )
 }
