6046e5cbe86d048821dcaf1d88c3d5fde3228123
div-leadeasy <diego-leadeasy@outlook.com.br>
2022-12-12 01:16:15 -0300

    fix: boleblaid cursos+router

--- aula_1/src/components/Button/index.tsx
+++
@@ -1,9 +0,0 @@
-import * as S from "./styles"
-
-const Button = () => (
-  <S.Container>
-    <S.Button>Button</S.Button>
-  </S.Container>
-)
-
-export default Button
--- aula_1/src/pages/index.tsx
+++
@@ -1,9 +0,0 @@
-import Aula_1 from '@pages/aula_1'
-import Aula_2 from '@pages/aula_2'
-
-export default function index() {
-  return (
-    // <Aula_1 />
-    <Aula_2 />
-  )
-}
